d7133305a9f2c521292e991fc270720c7cc7f834
Jalon Wong <jalonwong@gmail.com>
2024-01-19 23:18:45 +0800

    delete protobuf submodule

--- ci.py
+++ ci.py
@@ -3,98 +3,77 @@ import sys
 import os
 import shutil
 import zipfile
+import platform
 from glob import glob
 
 
 PYTHON = sys.executable
 
 
-def build_protoc(cmake: bool) -> None:
+def build_protoc() -> None:
     print("---------------------------------------------------------------------------")
     print("---- Build protoc ---------------------------------------------------------")
     print("---------------------------------------------------------------------------", flush=True)
-    cwd = os.getcwd()
-    if cmake:
-        os.makedirs("protobuf/solution", exist_ok=True)
-        os.chdir("protobuf/solution")
-        subprocess.run(
-            'cmake -G "Visual Studio 16 2019" -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=out ..',
-            shell=True,
-            check=True,
-        )
-        subprocess.run("msbuild protoc.vcxproj /property:Configuration=Release".split(), check=True)
-    else:
-        os.chdir("protobuf")
-        subprocess.run("bazel build --enable_bzlmod=false :protoc".split(), check=True)
-    os.chdir(cwd)
+    subprocess.run(
+        ["bazel", "build", "@com_google_protobuf//:protoc"],
+        check=True,
+    )
 
 
-def build_protobuf_c(cmake: bool) -> None:
+def build_protobuf_c() -> None:
     print("---------------------------------------------------------------------------")
     print("---- Build protobuf-c -----------------------------------------------------")
     print("---------------------------------------------------------------------------", flush=True)
-    cwd = os.getcwd()
-    if cmake:
-        p = os.path.abspath("protobuf/solution/out/bin")
-        os.makedirs("protobuf-c/build-cmake/build", exist_ok=True)
-        os.chdir("protobuf-c/build-cmake/build")
-        env = os.environ.copy()
-        env["PATH"] = p + ";" + env["PATH"]
-        subprocess.run("cmake -DCMAKE_INSTALL_PREFIX=out ..", shell=True, check=True, env=env)
-        subprocess.run("msbuild INSTALL.vcxproj /property:Configuration=Release".split(), check=True)
-    else:
-        src = glob("src/*")
-        for s in src:
-            if os.path.isfile(s):
-                shutil.copy(s, "protobuf-c/")
-
-        shutil.copy("src/.bazelrc", "protobuf-c/")
-        shutil.copy("src/protobuf-c/BUILD", "protobuf-c/protobuf-c/")
-
-        subprocess.run(
-            [
-                "protobuf/solution/Release/protoc.exe",
-                "-I=protobuf-c/protobuf-c",
-                "-I=protobuf/src",
-                "--cpp_out=protobuf-c/protobuf-c",
-                "protobuf-c.proto",
-            ],
-            check=True,
-        )
-        os.chdir("protobuf-c")
-        subprocess.run("bazel build all".split(), check=True)
-    os.chdir(cwd)
+    subprocess.run(
+        [
+            "bazel-bin/external/com_google_protobuf/protoc.exe",
+            "-I=protobuf-c",
+            "-I=bazel-protobuf-c/external/com_google_protobuf/src",
+            "--cpp_out=protobuf-c",
+            "protobuf-c.proto",
+        ],
+        check=True,
+    )
+    subprocess.run("bazel build all".split(), check=True)
 
 
 def test_protobuf_c() -> None:
     print("---------------------------------------------------------------------------")
     print("---- Test protobuf-c ------------------------------------------------------")
     print("---------------------------------------------------------------------------", flush=True)
-    cwd = os.getcwd()
     env = os.environ.copy()
-    env["PATH"] = os.path.abspath("./protobuf-c/bazel-bin") + ";" + env["PATH"]
+    env["PATH"] = os.path.abspath("./bazel-bin") + ";" + env["PATH"]
     subprocess.run(
         [
-            "protobuf/solution/Release/protoc.exe",
-            "-I=protobuf-c/t",
-            "--c_out=protobuf-c/t",
+            "bazel-bin/external/com_google_protobuf/protoc.exe",
+            "-I=t",
+            "--c_out=t",
             "test-proto3.proto",
         ],
         check=True,
         env=env,
     )
-    os.chdir("protobuf-c")
     subprocess.run("bazel test :test".split(), check=True)
-    os.chdir(cwd)
 
 
 if __name__ == "__main__":
     subprocess.run([PYTHON, "--version"])
     subprocess.run("bazel --version".split(), check=True)
 
-    build_protoc(True)
-    build_protobuf_c(False)
+    src = glob("src/*")
+    for s in src:
+        if os.path.isfile(s):
+            shutil.copy(s, "protobuf-c/")
+
+    shutil.copy("src/.bazelrc", "protobuf-c/")
+    shutil.copy("src/protobuf-c/BUILD", "protobuf-c/protobuf-c/")
+
+    cwd = os.getcwd()
+    os.chdir("protobuf-c")
+    build_protoc()
+    build_protobuf_c()
     test_protobuf_c()
 
+    os.chdir(cwd)
     with zipfile.ZipFile("protobuf-c-win64.zip", "w", zipfile.ZIP_DEFLATED) as zip_f:
         zip_f.write("protobuf-c/bazel-bin/protoc-gen-c.exe", "bin/protoc-gen-c.exe")
